4d90cc652f245d540a1142a84b9126a16d237da1
felipesilva-plank <felipe.silva@joinplank.com>
2023-04-06 15:35:45 -0300

    first commit

---
+++ model/Crew.ts
@@ -0,0 +1,10 @@
+class Crew{
+    name : string;
+    ID : number;
+    launchId : number;
+    crewCrewmanId: number;
+    constructor(name : string, ID : number){
+        this.name = name;
+        this.ID = ID;
+    }
+}
