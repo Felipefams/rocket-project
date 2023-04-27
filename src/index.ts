4c7164b1fabb37c28c6839eb4b5cd3a3b48ac4b8
felipesilva-plank <felipe.silva@joinplank.com>
2023-04-27 14:22:08 -0300

    removed dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import 'reflect-metadata'
 import express from 'express';
 // import "./database"
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
+// import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+// dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 8080;
 // const rocketApi = require("../routes/api/rocketApi")
