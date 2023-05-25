dd1cc92422a3e2e50e68444062839a8056390b0c
felipesilva-plank <felipe.silva@joinplank.com>
2023-05-25 14:59:22 -0300

    fixed backend post requests

--- src/controllers/CrewController.ts
+++ src/controllers/CrewController.ts
@@ -1,5 +1,29 @@
+import { Request, Response } from "express";
 import Crew from "../model/Crew";
 import { BaseController } from "./interfaces/BaseController";
+import { CrewmanController } from "./CrewmanController";
+import { crewService, crewmanController, crewmanService } from "../controllerFactory";
 
 export class CrewController extends BaseController<Crew>{
+    override create = async (req: Request, res: Response) => {
+        try {
+            let crewman = req.body.crewman;
+            if (typeof crewman[0] === 'number' ) {
+                const replaceCrewman = [];  
+                for (let i = 0; i < req.body.crewman.length; i++) {
+                    const tmpCrewman = await crewmanService.getById(req.body.crewman[i]);
+                    if (!tmpCrewman) {
+                        return res.status(404).send(`Crewman with id ${req.body.crewman[i]} not found`);
+                    }
+                    replaceCrewman.push(tmpCrewman);
+                }
+                crewman = replaceCrewman;
+            }
+            const newCrew = new Crew(req.body.name, crewman);
+            const obj = await crewService.create(newCrew);
+            return res.json(obj);
+        } catch (err) {
+            return res.status(500).send(`${err}`);
+        }
+    }
 }
--- src/controllers/LaunchController.ts
+++ src/controllers/LaunchController.ts
@@ -1,5 +1,36 @@
+import { Request, Response } from "express";
+import { crewService, launchService, rocketService } from "../controllerFactory";
 import Launch from "../model/Launch";
 import { BaseController } from "./interfaces/BaseController";
 
 export class LaunchController extends BaseController<Launch>{
+    override create = async (req: Request, res: Response) => {
+        try {
+            let crew = req.body.crew;
+            let rocket = req.body.rocket;
+            if(typeof crew === 'number' ) {
+                crew = await crewService.getById(crew);
+                if (!crew){
+                    return res.status(404).send(`Crew with id ${req.body.crew} not found`);
+                }
+            }
+            if(typeof rocket === 'number' ) {
+                rocket = await rocketService.getById(rocket);
+                if (!rocket){
+                    return res.status(404).send(`Rocket with id ${req.body.rocket} not found`);
+                }
+            }
+            const newLaunch = new Launch(
+                req.body.launchCode,
+                req.body.date,
+                req.body.success,
+                rocket,
+                crew,
+            );
+            const obj = await launchService.create(newLaunch);
+            return res.json(obj);
+        } catch (err) {
+            return res.status(500).send(`${err}`);
+        }
+    }
 }
